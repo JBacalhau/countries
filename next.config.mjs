b0241a167a4ddf95b600e38153fcdcf6fd7528b2
João Bacalhau <joao.bacalhau81@gmail.com>
2024-12-03 17:11:01 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  reactStrictMode: true, // Ativa o modo estrito do React
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'flagcdn.com', // Permite imagens deste domínio
+    
+    reactStrictMode: true, // Ativa o modo estrito do React
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'flagcdn.com', // Permite imagens deste domínio
+        },
+      ],
+    },
+    
+    env: {
+        API_URL: 'https://sua-api.com', // Exemplo
       },
-    ],
-  },
   
-  env: {
-    NEXT_PUBLIC_API_URL: 'https://restcountries.com/v3.1', // Configura variável de ambiente
-  },
-
 
 };
 
